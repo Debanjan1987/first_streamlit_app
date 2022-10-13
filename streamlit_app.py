c01011a328e0fab54932bd336da8e4a00631976b
Debanjan Das <114387278+Debanjan1987@users.noreply.github.com>
2022-10-13 17:41:49 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,7 +56,17 @@ if streamlit.button('Get Fruit Load List'):
       streamlit.dataframe(my_data_row)
 
 streamlit.stop()
-add_fruit = streamlit.text_input('What fruit would you like add?')
-streamlit.write('Thanks for adding', add_fruit)
-my_cur.execute("insert into fruit_load_list values('from streeamlit')")
+
+def insert_row_snowflake(new_fruit)
+       with my_cnx.cursor() as my_cur:
+            my_cur.execute("insert into fruit_load_list values('from streeamlit')")
+            return 'Thanks for adding'+new_fruit
+
+      add_fruit = streamlit.text_input('What fruit would you like add?')
+      if streamlit.button('Add fruit to the List'):
+            my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+            back_from_function = insert_row_snowflake(add_fruit)
+            streamlit.text(back_from_function)
+
+
 
